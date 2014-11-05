27c6a71fe60372759537bfca6289039856ca05e0
k2-hashimoto <k2-hashimoto@nri-net.com>
2014-11-05 16:49:50 +0900

    xcodeプロジェクト追加
    xcodeプロジェクト追加

---
+++ BeaconApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconApp
+//
+//  Created by k2-hashimoto on 2014/11/05.
+//  Copyright (c) 2014年 Takahashimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
