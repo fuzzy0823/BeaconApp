2862e318b04bd565f0590085689584c5539880f8
design <design@design-no-Mac-mini-2.local>
2014-11-11 17:45:05 +0900

    設定画面のセル設定

--- BeaconApp/SettingsViewController.swift
+++ BeaconApp/SettingsViewController.swift
@@ -12,44 +12,11 @@ import CoreBluetooth
 
 class SettingsViewController: UIViewController, CBPeripheralManagerDelegate,  UITextFieldDelegate{
     
+    
+
     @IBAction func goBack() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-    
-//    
-//    @IBOutlet var UuidText: UITextField!
-//    @IBOutlet var majorIdPicker1: UIPickerView!
-//    @IBOutlet var majorIdPicker2: UIPickerView!
-//    @IBOutlet var minorIdPicker1: UIPickerView!
-//    @IBOutlet var minorIdPicker2: UIPickerView!
-//    
-//    // LocationManager
-//    var myPheripheralManager:CBPeripheralManager!
-//    // Button
-//    let myButton: UIButton = UIButton()
-//    // UUID
-//    var myTextField: UITextField!
-//    // Window
-//    var myWindow: UIWindow!
-//    let myWindowButton = UIButton()
-//    // MajorId(上位)
-//    var myMajorId1: NSString = "0"
-//    // MajorId(下位)
-//    var myMajorId2: NSString = "0"
-//    // MinorId(上位)
-//    var myMinorId1: NSString = "0"
-//    // MinorId(下位)
-//    var myMinorId2: NSString = "0"
-//    
-//    // UUID
-//    var myUuid: NSString = "CB86BC31-05BD-40CC-903D-1C9BD13D966A"
-//    
-//    
-//    
-//    // 表示する値の配列.
-//    let myValues: NSArray = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-//    let myUuids: NSArray = ["CB86BC31-05BD-40CC-903D-1C9BD13D966A"]
-    
     
     
     override func viewDidLoad() {
