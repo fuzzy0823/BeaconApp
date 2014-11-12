27cded697a3d73a402f52ee881b0e34864d339d6
stnydesign <shun.takahashi0728@gmail.com>
2014-11-12 17:29:56 +0900

    Macでログイン・ログアウトするapplescript追加。beacon.jsを起動したタイミングでMacをロックする処理追加。(実際はアンロックさせる)

--- bleacon/beacon.js
+++ bleacon/beacon.js
@@ -1,6 +1,8 @@
+var exec = require('child_process').exec;
 Bleacon = require('bleacon');
 Bleacon.startScanning();
+exec('osascript LockMac.scpt',function(err,stdout,stderr){ });
 
-Bleacon.on('discover', function(bleacon) {
-   console.dir(bleacon);
+Bleacon.on('discover',function(bleacon){
+    console.dir(bleacon);
 });
