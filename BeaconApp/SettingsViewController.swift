9d16eda129b6b336423c91180c4b2e86ba9f8718
design <design@design-no-Mac-mini-2.local>
2014-11-13 19:16:56 +0900

    まだひどい有様だがとりあえず項目の項目とフォームだけ作成 （どうやって紐付けするのか…）

--- BeaconApp/SettingsViewController.swift
+++ BeaconApp/SettingsViewController.swift
@@ -10,107 +10,151 @@ import UIKit
 import CoreLocation
 import CoreBluetooth
 
-class SettingsViewController: UIViewController, CBPeripheralManagerDelegate,  UITextFieldDelegate{
-    
-    
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource , UITextFieldDelegate{
 
     @IBAction func goBack() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+
+    // Tableで使用する配列を定義する.
+//    let myiPhoneItems: NSArray = ["iOS8"]
+//    let myAndroidItems: NSArray = ["5.x"]
+//    let myAndroidItems2: NSArray = ["1.x"]
     
+    // Sectionで使用する配列を定義する.
+    let mySections: NSArray = ["UUID", "MajorID", "MinorID"]
     
     override func viewDidLoad() {
-    }
-    
-    
-    
-    /*
-    ボタンイベント
-    */
-    func onClickMyButton(sender: UIButton){
+        super.viewDidLoad()
         
+        // Status Barの高さを取得する.
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
         
-    }
-    
-    /*
-    Windowの自作
-    */
-    func makeMyWindow(){
+        // Viewの高さと幅を取得する.
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        // TableViewの生成( status barの高さ分ずらして表示 ).
+        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y: barHeight + 50, width: displayWidth, height: displayHeight - barHeight))
+        
+        // Cell名の登録をおこなう.
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceの設定をする.
+        myTableView.dataSource = self
+        // Delegateを設定する.
+        myTableView.delegate = self
+        // Viewに追加する.
+        self.view.addSubview(myTableView)
 
+//        let myTextField: UITextField = UITextField(frame: CGRectMake(0,0,200,30))
+//        // 表示する文字を代入する.
+//        myTextField.text = "Hello Swift!!"
+//        // Delegateを設定する.
+//        myTextField.delegate = self
+//        // 枠を表示する.
+//        myTextField.borderStyle = UITextBorderStyle.RoundedRect
+//        // UITextFieldの表示する位置を設定する.
+//        myTextField.layer.position = CGPoint(x:self.view.bounds.width/2,y:100);
+//        // Viewに追加する.
+//        self.view.addSubview(myTextField)
     }
     
-    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*
+    セクションの数を返す.
+    */
+    func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return mySections.count
+    }
+    
+    /*
+    セクションのタイトルを返す.
+    */
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return mySections[section] as? String
+    }
+    
+    /*
+    Cellが選択された際に呼び出される.
+    */
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if indexPath.section == 0 {
+//            println("Value: \(myiPhoneItems[indexPath.row])")
+        } else if indexPath.section == 1 {
+//            println("Value: \(myAndroidItems[indexPath.row])")
+        } else if indexPath.section == 2 {
+//            println("Value: \(myAndroidItems2[indexPath.row])")
+        }
+    }
+    
+    /*
+    テーブルに表示する配列の総数を返す.
+    */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+//            return myiPhoneItems.count
+        } else if section == 1 {
+//            return myAndroidItems.count
+        } else if section == 2 {
+//            return myAndroidItems2.count
+        }
+        
+        return 1
+    }
+    
+    /*
+    Cellに値を設定する.
+    */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as UITableViewCell
+        
+        if indexPath.section == 0 {
 
+            let uuidField: UITextField = UITextField(frame: CGRectMake(0,0,300,30))
+            uuidField.text = "CB86BC31-05BD-40CC-903D-1C9BD13D966A"
+            uuidField.delegate = self
+            uuidField.borderStyle = UITextBorderStyle.None
+            uuidField.layer.position = CGPoint(x:self.view.bounds.width/2,y:120);
+            self.view.addSubview(uuidField)
+//            cell.textLabel.text = "\(myiPhoneItems[indexPath.row])"
+
+        } else if indexPath.section == 1 {
+            let majoridField: UITextField = UITextField(frame: CGRectMake(0,0,300,30))
+            majoridField.text = "FF"
+            majoridField.delegate = self
+            majoridField.borderStyle = UITextBorderStyle.None
+            majoridField.layer.position = CGPoint(x:self.view.bounds.width/2,y:190);
+            self.view.addSubview(majoridField)
+//            cell.textLabel.text = "\(myAndroidItems[indexPath.row])"
+
+        } else if indexPath.section == 2 {
+            let minoridField: UITextField = UITextField(frame: CGRectMake(0,0,300,30))
+            minoridField.text = "FF"
+            minoridField.delegate = self
+            minoridField.borderStyle = UITextBorderStyle.None
+            minoridField.layer.position = CGPoint(x:self.view.bounds.width/2,y:260);
+            self.view.addSubview(minoridField)
+//            cell.textLabel.text = "\(myAndroidItems2[indexPath.row])"
+
+        }
+        return cell
     }
-    
-    func peripheralManagerDidStartAdvertising(peripheral: CBPeripheralManager!, error: NSError!) {
-        
+    func textFieldShouldEndEditing(textField: UITextField!) -> Bool {
+        return true;
     }
-    
-//    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-//        
-//        return 1
-//        
-//    }
-    
-    /*
-    フォントを設定
-    */
-//    func pickerView(pickerView: UIPickerView!, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView! {
-//        
-//        return lab
-//        
-//    }
-    
-    /*
-    表示するデータ数.
-    */
-//    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        
-//        return myValues.count
-//        
-//    }
-    
-    /*
-    値を代入.
-    */
-//    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String {
-//        
-//        return myValues[row] as String
-//        
-//    }
-    
-    /*
-    Pickerが選択された際に呼ばれる.
-    */
-//    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//
-//    }
-    
-    /*
-    UITextFieldが編集開始された直後に呼ばれる
-    */
-    func textFieldDidBeginEditing(textField: UITextField!){
-        
-    }
-    
-    /*
-    UITextFieldが編集終了する直前に呼ばれる
-    */
-//    func textFieldShouldEndEditing(textField: UITextField!) -> Bool {
-//        
-//        return true;
-//        
-//    }
     
     /*
     改行ボタンが押された際に呼ばれる
     */
-//    func textFieldShouldReturn(textField: UITextField!) -> Bool {
-//        
-//        textField.resignFirstResponder()
-//        
-//        return true;
-//        
-//    }
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
 }
